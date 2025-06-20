2969274e9ddc3519bdc18d6f46c750eef8eaeeba
siddharthshiv1201 <shivsiddharth1201@gmail.com>
2025-06-20 09:58:45 +0530

    done

---
+++ javascript/file1.js
@@ -0,0 +1,34 @@
+
+
+// VARIABLE AND THERE TYPES
+
+let age=21;
+let myname="Siddharth Shiv";
+let promoted=true;
+let city=null;
+let score;
+// console.log(age);
+// console.log(myname);
+// console.log(promoted);
+// console.log(city);
+// console.log(score);
+// console.log(typeof age);
+// console.log(typeof myname);
+// console.log(typeof promoted);
+// console.log(typeof city);
+// console.log(typeof score);
+
+// let strAge=String(age);
+// console.log(typeof strAge);
+// console.log(strAge);
+
+// let rating="5";
+// console.log(typeof rating);
+// console.log(Number(rating));
+// console.log(typeof Number(rating));
+
+let flag="true";
+console.log(typeof flag);
+console.log(Boolean(flag));
+console.log(Number(flag));
+console.log(typeof Number(flag));
